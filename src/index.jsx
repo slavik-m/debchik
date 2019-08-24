dd6e934f132856595275dcacb262bd1a6572d140
slavik <relogn@mail.com>
2019-08-24 21:29:17 +0300

    Redux persist

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './components/client/App';
 
-import store from './state/store';
+import store, { persistor } from './state/store';
 
 // require('$assets/fonts.css');
 
 render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
--- src/state/store/index.js
+++ src/state/store/index.js
@@ -3,8 +3,15 @@ import {
 } from 'redux/es/redux';
 import createSagaMiddleware from 'redux-saga';
 import { setAutoFreeze } from 'immer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage/session';
 import runSagas from '../sagas/runSagas';
 import game from './game/reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
 if (process.env.NODE_ENV === 'production') {
   setAutoFreeze(false);
@@ -19,11 +26,15 @@ const composeEnhancers = (process.env.NODE_ENV !== 'production'
 
 const sagaMiddleware = createSagaMiddleware();
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeEnhancers(applyMiddleware(sagaMiddleware)),
 );
+
 
 runSagas(sagaMiddleware);
 
 export default store;
+export const persistor = persistStore(store);
