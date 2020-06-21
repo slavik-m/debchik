03786a0092a82ea8552822f0dd5c7cdebf3393d6
slavik-m <relogn@gmail.com>
2020-06-21 10:14:56 +0300

    Update path

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ require('$helpers/workbox');
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+    navigator.serviceWorker.register('service-worker.js').then((registration) => {
       console.log('SW registered: ', registration);
     }).catch((registrationError) => {
       console.log('SW registration failed: ', registrationError);
